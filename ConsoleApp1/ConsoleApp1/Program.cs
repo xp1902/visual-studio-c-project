06df21352b709a9130ac6313f92516d28066a626
shemlo <2911577624@qq.com>
2018-10-12 10:30:50 +0800

    initial release

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.Write("enter a number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1);
+
+        }
+    }
+}
